1f6230d0c7b97c3ba7741a5251554bb9a4dac66a
Anna Dybas <a.m.dybas@gmail.com>
2020-03-24 09:54:54 -0400

    Done.

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,6 @@
 # Implement your procedural solution here!
+def sum_square_difference(number)
+    sum_nums = (1..number).reduce(:+) ** 2
+    sq_nums = (1..number).map { |n| n ** 2 }.sum
+    sum_nums - sq_nums
+end
